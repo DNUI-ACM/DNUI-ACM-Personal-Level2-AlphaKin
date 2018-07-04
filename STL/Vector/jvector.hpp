40613b129aca46b7f54da1f9b5addfe1b6e50107
AlphaKin <34909962+AlphaKin@users.noreply.github.com>
2018-07-04 20:32:59 +0800

    Update jvector.hpp

--- STL/Vector/jvector.hpp
+++ STL/Vector/jvector.hpp
@@ -3,22 +3,22 @@
 #define DEFAULT_CAPACITY 4 //默认空间大小
 #define DEFAULT_INCREASE 2 //默认扩增倍数
 #include <cstddef>
-#include <malloc.h>
+#include <stdexcept>
 #include <cstring>
+#include <cstdio>
 
 namespace jj{
 	template<class T>
 	class jvector{
 	public:
-		using size_type = size_t;
-		using difference_type = ptrdiff_t;
-		using value_type = T;
-		using pointer = value_type* ;
-		using reference = value_type& ;
+		using size_type 		= size_t;
+		using difference_type 	= ptrdiff_t;
+		using value_type 		= T;
+		using pointer 			= value_type* ;
+		using reference 		= value_type& ;
 
-	protected:
-		T * start;
-		T * finish;
+	private:
+		T * base;
 		T * end_of_storage;
 	public:
 		//迭代器类
@@ -56,8 +56,8 @@ namespace jj{
 			reference operator*(){ return *ptr; }
 			reverse_iterator operator++(){ return --ptr; }
 			reverse_iterator operator--(){ return ++ptr; }
-			reverse_iterator operator+(const size_type & diff){ return ptr+diff; }
-			reverse_iterator operator-(const size_type & diff){ return ptr-diff; }
+			reverse_iterator operator+(const size_type & diff){ return ptr-diff; }
+			reverse_iterator operator-(const size_type & diff){ return ptr+diff; }
 			void operator++(int){
 				T * tmp = ptr;
 				--ptr;
@@ -74,137 +74,134 @@ namespace jj{
 		size_type len;
 		size_type cap;
 		pointer increase(const value_type & value){
-			pointer new_start = new value_type[cap * DEFAULT_INCREASE];
-			memmove(new_start, start, sizeof(T) * len);
-			delete start;
+			pointer new_base = new value_type[cap * DEFAULT_INCREASE];
+			memmove(new_base, base, sizeof(T) * len);
+			delete [] base;
 			cap *= DEFAULT_INCREASE;
-			end_of_storage = new_start + cap;
-			return new_start;
+			end_of_storage = new_base + cap;
+			return new_base;
 		}
 	public:
 		//默认构造
 		explicit jvector(const size_type c = DEFAULT_CAPACITY) : cap(c), len(0) {
-			start = new value_type[cap];
-			finish = start + len;
-			end_of_storage = start + cap;
+			base = new value_type[cap];
+			end_of_storage = base + cap;
 		}
 		//拷贝构造
 		jvector(const jvector & oth){
-			start = new value_type[oth.capacity()];
-			memmove(start, oth.begin(), oth.capacity() * sizeof(T));
+			base = new value_type[oth.capacity()];
+			iterator oth_base = oth.begin();
+			for(int i=0, aim=oth.size(); i<aim; ++i)
+				*(base + i) = *(oth_base + i);
 			len = oth.size();
 			cap = oth.capacity();
-			finish = start + len;
-			end_of_storage = start + cap;
+			end_of_storage = base + cap;
 		}
 		//初始化n个对象
 		jvector(size_type n, const T & value) : len(n), cap(n + 1){
-			start = new value_type[n + 1];
-			for(int i=0; i<n; ++i) *(start + i) = value;
-			finish = start + len;
-			end_of_storage = start + cap;
+			base = new value_type[n + 1];
+			for(int i=0; i<n; ++i) *(base + i) = value;
+			end_of_storage = base + cap;
 		}
 
-		~jvector(){ delete [] start; }
+		~jvector(){ delete [] base; }
 
 		void setlen(const int l){ len = l; }
 		void setcap(const int c){ cap = c; }
 
-		pointer begin() const { return start; }
+		iterator begin() const { return iterator(base); }
 		
-		pointer end() const { return finish; }
+		iterator end() const { return iterator(base + len); }
 
-		pointer rbegin() const { return finish-1; }
+		reverse_iterator rbegin() const { return reverse_iterator(base + len - 1); }
 		
-		pointer rend() const { return start-1; }
+		reverse_iterator rend() const { return reverse_iterator(base - 1); }
 		
 		size_type size() const { return len; }
 		
 		size_type capacity() const { return cap; }
 		
 		bool empty() const { return len == 0; }
+		         
+		reference front() const { return *base; }
 		
-		reference front() const { return *start; }
-		
-		reference back() const { return *(finish - 1); }
+		reference back() const { return *(base + len - 1); }
 		
 		void clear(){ while(size()) pop_back(); }
 		
 		reference operator[](size_type position){
-			if(position < len) return *(start + position);
+			if(position < len) return *(base + position);
 		}
 
-		pointer at(const size_type position){
-			if(position < len) return start + position;
+		reference at(const size_type position){
+			if(position < len) return *(base + position);
 		}
 
-		pointer push_back(const T & value){
-			if(len == cap) start = increase(value);
-			*(start + len++) = value;
-			finish = start + len;
-			return finish - 1;
+		void push_back(const T & value){
+			if(len == cap) base = increase(value);
+			*(base + len++) = value;
+
 		}
 		
 		void pop_back(){
 			if(empty()) return;
-			--len, --finish;
+			--len;
 		}
 		
 		pointer erase(pointer position){
-			if(position < start + len){
+			if(position < base + len){
 				pointer tmp = position;
 				while(position != end()){
 					*position = *(position + 1);
 					++position;
 				}
-				--len, --finish;
+				--len;
 				return tmp;
 			}
 			else return nullptr;
 		}
 
 		pointer erase(size_type spos, size_type epos){
-			if(spos < 0 || epos >= len) return start + spos;
+			if(spos < 0 || epos >= len) return base + spos;
 			for(int i=0, p=epos, q=spos, aim = len-epos; i<aim; ++i)
-				*(start + q++) = *(start + p++);
+				*(base + q++) = *(base + p++);
 			len -= epos - spos;
-			finish = start + len - epos + spos;
-			return start + spos;
+			return base + spos;
 		}
 
 		pointer erase(size_type position){
 			if(position < len){
-				T * tmp = start + position;
+				T * tmp = base + position;
 				while(position < len){
-					*(start + position) = *(start + position + 1);
+					*(base + position) = *(base + position + 1);
 					++position;
 				}
-				--len, --finish;
+				--len;
 				return tmp;
 			}
 			else return nullptr;
 		}
 		
-		void swap(jvector & obj){
-			pointer tmpit = obj.start; obj.start = start; start = tmpit;
-			tmpit = obj.finish; obj.finish = finish; finish = tmpit;
-			tmpit = obj.end_of_storage; obj.end_of_storage = end_of_storage; end_of_storage = end_of_storage;
-			size_type tmp = len; len = obj.size(); obj.setlen(tmp);
-			tmp = cap; cap = obj.capacity(); obj.setcap(tmp);
-		}
+		// void swap(jvector & obj){
+		// 	pointer tmpit = obj.base; obj.base = base; base = tmpit;
+		// 	tmpit = obj.finish; obj.finish = finish; finish = tmpit;
+		// 	tmpit = obj.end_of_storage; obj.end_of_storage = end_of_storage; end_of_storage = end_of_storage;
+		// 	size_type tmp = len; len = obj.size(); obj.setlen(tmp);
+		// 	tmp = cap; cap = obj.capacity(); obj.setcap(tmp);
+		// }
 
 		void resize(size_type new_size, const T & x){
 			pointer new_space;
 			if(new_size != cap){
 				new_space = new value_type[new_size];
-				memmove(new_space, start, sizeof(T) * new_size);
+				memmove(new_space, base, sizeof(T) * new_size);
 			} 
-			delete start;
-			start = new_space;
-			pointer tmp = start + (len < new_size ? len : new_size);
+			delete base;
+			base = new_space;
+			pointer tmp = base + (len < new_size ? len : new_size);
 			len = cap = new_size;
-			end_of_storage = finish = start + len;
-			while(tmp != finish){
+			end_of_storage = base + len;
+			while(tmp != base + len){
 				*tmp = x;
 				++tmp;
 			}
